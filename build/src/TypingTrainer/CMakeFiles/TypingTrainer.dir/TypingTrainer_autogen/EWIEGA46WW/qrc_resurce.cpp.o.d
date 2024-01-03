9cee155477adb55fd5e733c4263447457371f6c1
mykhailo <mykhailo@kali.mukhailo>
2024-01-03 13:09:05 +0200

    Added the option to select the program assembly together with the tests.

--- build/src/TypingTrainer/CMakeFiles/TypingTrainer.dir/TypingTrainer_autogen/EWIEGA46WW/qrc_resurce.cpp.o.d
+++
@@ -1,3 +0,0 @@
-src/TypingTrainer/CMakeFiles/TypingTrainer.dir/TypingTrainer_autogen/EWIEGA46WW/qrc_resurce.cpp.o: \
- /home/mykhailo/Documents/TypingTrainer/build/src/TypingTrainer/TypingTrainer_autogen/EWIEGA46WW/qrc_resurce.cpp \
- /usr/include/stdc-predef.h
